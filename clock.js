317363a98c17b7a2d815a7fc89e03afcdf093e65
Thomas Gilbert <tcgilbert@tg-MBP.local>
2020-10-23 12:41:44 -0400

    add ticking second hand

--- clock.js
+++ clock.js
@@ -3,19 +3,16 @@ const secondHand = document.getElementById('second');
 
 var x = setInterval(function() {
     seconds();
-    console.log(secondsCounter);
-    return secondsCounter;
+    secondRotation(secondsCounter);
 }, 1000);
-
- 
 
 
 function seconds() {
     secondsCounter++;
 }
 
-
 function secondRotation(seconds) {
     let degRotation = seconds * 6;
-    return degRotation;
+    console.log(degRotation);
+    secondHand.style.transform = `rotate(${degRotation}deg)`;
 }
